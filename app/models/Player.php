dc2cdc94d875a7225c4adb9a52f5639f95fdc094
Nutic <lordmilutin@gmail.com>
2014-06-03 00:25:18 +0200

    Teams and team pages done

--- app/models/Player.php
+++ app/models/Player.php
@@ -1,12 +1,14 @@
 <?php
 
+
 class Player extends Eloquent {
 
-    protected $table = 'players';
+	protected $table = 'players'; 
     protected $primaryKey = 'playerID';
+    protected $fillable = array('userID', 'name', 'last_name', 'teamID', 'bio', 'country', 'facebook', 'twiter', 'website');
 
-    public function user() {
+    public function user()
+    {
         return $this->belongsTo('User', 'userID', 'userID');
     }
-
 }
