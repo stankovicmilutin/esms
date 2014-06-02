2af2e71272aba1c5543a608720efa88e0c9cd638
Nutic <lordmilutin@gmail.com>
2014-06-03 00:50:48 +0200

    Merge remote-tracking branch 'origin/NetBeans'
    Conflicts:
    	app/routes.php

--- app/models/Player.php
+++ app/models/Player.php
@@ -1,31 +1,13 @@
-<<<<<<< HEAD
 <?php
-
 
 class Player extends Eloquent {
 
-	protected $table = 'players'; 
+    protected $table = 'players';
     protected $primaryKey = 'playerID';
     protected $fillable = array('userID', 'name', 'last_name', 'teamID', 'bio', 'country', 'facebook', 'twiter', 'website');
 
-    public function user()
-    {
+    public function user() {
         return $this->belongsTo('User', 'userID', 'userID');
     }
+
 }
-=======
-<?php
-
-
-class Player extends Eloquent {
-
-	protected $table = 'players'; 
-    protected $primaryKey = 'playerID';
-    protected $fillable = array('userID', 'name', 'last_name', 'teamID', 'bio', 'country', 'facebook', 'twiter', 'website');
-
-    public function user()
-    {
-        return $this->belongsTo('User', 'userID', 'userID');
-    }
-}
->>>>>>> 4605b1b0b128d74e546cb2959f133a4ebb1d1964
