85bfa58b56dd4fd3a7ef631a2a97bc2fe6fec3bf
dejan7 <dejanstosic7@gmail.com>
2014-06-17 01:08:48 +0200

    zapocet brackets

--- app/views/tournaments/treeview.blade.php
+++ app/views/tournaments/treeview.blade.php
@@ -3,19 +3,42 @@
     foreach ($matches as $match) {
         $matchArray[$match->matchID] = $match;
     }
-    genTree($final, $matchArray);
+    /*genTree($final, $matchArray);*/
+    $currentMatch = $matchArray[$final];
 
-    function genTree($finalMatch, $matchArray) {
+    $red = new SplQueue();
+    $c = 1;
+    echo '<table class="bracketsTable"><tr>';
+    while ($currentMatch) {
+      if ($c == 1 || $c == 2 || $c == 4 || $c == 8 || $c == 17 || $c == 33)
+      echo '<td style="padding-right:30px">';
+      printMatch($currentMatch);
+      
+      if ($c == 1 || $c == 3 || $c == 7 || $c == 15 || $c == 31 || $c == 63)
+      echo '</td>';
+      $c++;
+
+      $red->enqueue($currentMatch->child_match_a);
+      $red->enqueue($currentMatch->child_match_b);
+      
+      $i = $red->dequeue();
+      if ($i)
+        $currentMatch = $matchArray[$i];
+      else
+        $currentMatch = NULL;
+    }
+    echo '</table></tr>';
+    /*function genTree($finalMatch, $matchArray) {
         $match = $matchArray[$finalMatch];
         printMatch($match);
         if ($match->child_match_a != NULL && $match->child_match_b != NULL) {
             genTree($match->child_match_a, $matchArray);
             genTree($match->child_match_b, $matchArray);
         }
-    }
+    }*/
 
     function printMatch($match) {
-        echo '<div class="col-md-2">
+        echo '<div>
                 <div class="panel panel-warning">
                     <div class="panel-heading">
                         <h3 class="panel-title">'. $match->tournament_phase .'</h3>
@@ -23,10 +46,14 @@
                     <div class="panel-body">';
         
                 if($match->hostTeam != null)
-                    echo $match->hostTeam->name,"<hr>";
+                    echo $match->hostTeam->name;
+                else
+                  echo 'TBA';
+                echo "<hr>";
                 if($match->guestTeam != null)
                     echo $match->guestTeam->name;
-               
+                else
+                  echo 'TBA';
         echo  '     </div>
                 </div>
               </div>';
